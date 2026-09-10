177261769cea4ba174e944c9e92443c9387ffa7a
Vsingla34 <vishalsingla.singla@gmail.com>
2026-09-10 12:08:14 +0530

    Changes 1

--- src/pages/admin/Adminblogtab.jsx
+++ src/pages/admin/Adminblogtab.jsx
@@ -88,4 +88,4 @@ export default function AdminBlogTab({
       })()}
     </div>
   );
-}
+}   
--- src/pages/admin/Adminjobapplicationstab.jsx
+++ src/pages/admin/Adminjobapplicationstab.jsx
@@ -79,4 +79,4 @@ export default function AdminJobApplicationsTab({
       )}
     </div>
   );
-} 
+}
